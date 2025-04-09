faa34abaf0b45145aa8d383cc1de725f93adca05
Andree Wenas <andreewenas@gmail.com>
2025-04-09 21:50:02 +0700

    Update Layanan.tsx

--- src/components/Layanan.tsx
+++ src/components/Layanan.tsx
@@ -9,7 +9,7 @@ const projects = [
       link: "https://stripe.com",    
     }, 
     {
-      title: "Custom Velg",
+      title: "Custom Velg", 
       description:
         "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
       link: "https://netflix.com",
